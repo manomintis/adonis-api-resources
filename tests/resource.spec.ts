f6aa583ed705f02a77de574a09138f5774a595a0
manomintis <{ID}+{username}@users.noreply.github.com>
2024-06-19 14:40:10 +0300

    Adjust unit tests for recent changes

--- tests/resource.spec.ts
+++ tests/resource.spec.ts
@@ -1,3 +1,4 @@
+import { serialize } from 'node:v8'
 import { Resource } from '../src/resource.js'
 import { test } from '@japa/runner'
 
@@ -5,6 +6,8 @@ class TestResourceResource extends Resource {
   defineMap(data: any): object {
     return {
       fullName: data.firstName + ' ' + data.lastName,
+      firstName: data.firstName,
+      lastName: data.lastName,
     }
   }
 }
@@ -18,25 +21,46 @@ const testObjectCollection = [
   {
     firstName: 'John',
     lastName: 'Doe',
+    serialize: () => {
+      return {
+        firstName: 'John',
+        lastName: 'Doe',
+      }
+    },
   },
   {
     firstName: 'Jane',
     lastName: 'Doe',
   },
+  {
+    firstName: 'John1',
+    lastName: 'Doe1',
+  },
+  {
+    firstName: 'Jane1',
+    lastName: 'Doe1',
+  },
+  {
+    firstName: 'John2',
+    lastName: 'Doe2',
+  },
+  {
+    firstName: 'Jane2',
+    lastName: 'Doe2',
+  },
+  {
+    firstName: 'John3',
+    lastName: 'Doe3',
+  },
+  {
+    firstName: 'Jane3',
+    lastName: 'Doe3',
+  },
 ]
 
 class ObjectPaginated {
   currentPage = 1
-  rows = [
-    {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-  ]
+  rows = testObjectCollection
   getMeta() {
     return { currentPage: 1 }
   }
@@ -46,6 +70,16 @@ const testObjectPaginated = new ObjectPaginated()
 const resultEntity: any = new TestResourceResource(testObjectEntity).refine()
 const resultCollection: any = new TestResourceResource(testObjectCollection).refine()
 const resultPaginated: any = new TestResourceResource(testObjectPaginated).refine()
+const resultRefinedPaginated: any = new TestResourceResource(testObjectCollection).refinePaginate(
+  1,
+  4
+)
+const resultRefinedPaginated2: any = new TestResourceResource(testObjectCollection).refinePaginate(
+  2,
+  4
+)
+const resultPick: any = new TestResourceResource(testObjectCollection).pick('firstName')
+const resultOmit: any = new TestResourceResource(testObjectCollection).omit('firstName')
 
 test.group('Resource', () => {
   test('test_entity', ({ assert }) => {
@@ -59,5 +93,23 @@ test.group('Resource', () => {
     assert.equal(resultPaginated.data[0].fullName, 'John Doe')
     assert.equal(resultPaginated.data[1].fullName, 'Jane Doe')
     assert.equal(resultPaginated.meta.currentPage, 1)
+  })
+  test('test_refine_pagination', ({ assert }) => {
+    assert.equal(resultRefinedPaginated.data[0].fullName, 'John Doe')
+    assert.equal(resultRefinedPaginated.data[1].fullName, 'Jane Doe')
+    assert.equal(resultRefinedPaginated.meta.currentPage, 1)
+  })
+  test('test_refine_pagination2', ({ assert }) => {
+    assert.equal(resultRefinedPaginated2.data[0].fullName, 'John2 Doe2')
+    assert.equal(resultRefinedPaginated2.data[1].fullName, 'Jane2 Doe2')
+    assert.equal(resultRefinedPaginated2.meta.currentPage, 2)
+  })
+  test('test_pick', ({ assert }) => {
+    assert.equal(resultPick.data[0].firstName, 'John')
+    assert.equal(resultPick.data[0].lastName, undefined)
+  })
+  test('test_omit', ({ assert }) => {
+    assert.equal(resultOmit.data[0].firstName, undefined)
+    assert.equal(resultOmit.data[0].lastName, 'Doe')
   })
 })
