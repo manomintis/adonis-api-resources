b6ba12c465a655b8b5ce4008e686404700378f58
manomintis <{ID}+{username}@users.noreply.github.com>
2024-06-19 13:20:19 +0300

    Implement pick, omit, redefine, pagination

--- src/resource.ts
+++ src/resource.ts
@@ -8,41 +8,98 @@
  */
 
 export abstract class Resource {
-  private data: any
+  protected data: any
 
-  // Constructor
   constructor(data: object) {
     this.data = data
   }
 
-  // Require explicit map definition
   abstract defineMap(data: any): object
 
-  // Prepare refined object
-  refine_entity(data: any): object {
+  private isPaginated(): boolean {
+    return 'rows' in this.data && 'currentPage' in this.data
+  }
+
+  private isCollection(): boolean {
+    return Array.isArray(this.data)
+  }
+
+  private redefineEntity(data: any): object {
     return this.defineMap(data)
   }
 
-  // Prepare refined collection
-  refine_collection(data: any[]): object {
+  private redefineCollection(data: any[]): object {
     return data.map((item) => {
-      return this.refine_entity(item)
+      return this.redefineEntity(item)
     })
   }
 
-  // Return refined output
-  refine(): object {
-    if ('rows' in this.data && 'currentPage' in this.data) {
+  pick(...keys: string[]): this {
+    let result = []
+    for (let item of this.data) {
+      result.push(Object.assign({}, ...keys.map((key) => ({ [key]: item[key] }))))
+    }
+    this.data = result
+    return this
+  }
+
+  omit(...keys: string[]): this {
+    let result = []
+    const exclude = new Set(keys)
+    for (let item of this.data) {
+      const o = typeof item.serialize === 'function' ? item.serialize() : item
+      result.push(Object.fromEntries(Object.entries(o).filter((e) => !exclude.has(e[0]))))
+    }
+    this.data = result
+    return this
+  }
+
+  redefine(): this {
+    if (this.isPaginated()) {
       const meta = (this.data as any).getMeta()
-      const collection = this.refine_collection((this.data as any).rows)
-      const result = {
+      const collection = this.redefineCollection((this.data as any).rows)
+      this.data = {
         meta: meta,
         data: collection,
       }
-      return result
-    } else if (Array.isArray(this.data)) {
-      return this.refine_collection(this.data)
+    } else if (this.isCollection()) {
+      this.data = this.redefineCollection(this.data)
+    } else {
+      this.data = this.redefineEntity(this.data)
     }
-    return this.refine_entity(this.data)
+    return this
+  }
+
+  refine(): object {
+    return this.redefine().get()
+  }
+
+  refinePaginate(page: number = 1, limit: number = 10): object {
+    return this.redefine().paginate(page, limit)
+  }
+
+  paginate(page: number = 1, limit: number = 10) {
+    let lastPage = Math.max(Math.ceil(this.data.length / limit), 1)
+    const meta = {
+      total: this.data.length,
+      perPage: limit,
+      currentPage: page,
+      lastPage: lastPage,
+      firstPage: 1,
+      firstPageUrl: '/?page=1',
+      lastPageUrl: '/?page=' + lastPage,
+      nextPageUrl: page < lastPage ? `/?page=${page + 1}` : null,
+      previousPageUrl: page > 1 ? `/?page=${page - 1}` : null,
+    }
+    const collection = this.data.slice((page - 1) * limit, page * limit)
+    this.data = {
+      meta: meta,
+      data: collection,
+    }
+    return this.data
+  }
+
+  get() {
+    return this.data
   }
 }
